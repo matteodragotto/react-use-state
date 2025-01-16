dbcbb197a0601dc8914bcdaa45ced2467b2b4325
matteodragotto <matteodrag8@gmail.com>
2025-01-16 13:21:44 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <p>Learn Web development</p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import languages from "../../data/languages"
+
+const Main = () => {
+
+  return (
+    <main>
+      <ul>
+        <li><button>AAAA</button></li>
+        <li><button>AAAA</button></li>
+        <li><button>AAAA</button></li>
+        <li><button>AAAA</button></li>
+        <li><button>AAAA</button></li>
+        <li><button>AAAA</button></li>
+      </ul>
+    </main>
+  )
+}
+
+export default Main
